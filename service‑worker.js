270dd46f33989bad16497bb0a88ea0bd0f27be7d
Nfk1 <110776782+Nfk1@users.noreply.github.com>
2025-12-11 12:50:41 +0300

    Add files via upload

---
+++ service‑worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/style.css',
+  '/items.json',
+  '/manifest.json',
+  '/product.html',
+  '/product.js',
+  // Добавьте другие важные ресурсы
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
